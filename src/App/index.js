72d3bc5659ca1fe4186ff321519392fcf1739618
shadowbeat274 <olivertate274@gmail.com>
2023-03-17 21:16:11 -0600

    localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -2,17 +2,29 @@ import React        from 'react'
 import { IndexUI }  from './indexUI'
 import './App.css';
 
-const todosDefault = [
-  { text: 'Cortar Cebollas', completed: false },
-  { text: 'Study Platxi', completed: true },
-  { text: 'Cry with the Llorona', completed: false }
-]
+// const defaultTodos = [
+//   { text: 'Cortar Cebollas', completed: false },
+//   { text: 'Study Platxi', completed: true },
+//   { text: 'Cry with the Llorona', completed: false }
+// ]
 
 function App() {
+  debugger
+  const getTodosLocalStorage = window.localStorage.getItem( 'TODOS_V1' )
+  let newLocalTodos
+
+  if( !getTodosLocalStorage ){    
+    
+    window.localStorage.setItem( 'TODOS_V1', '[]' )
+    newLocalTodos = []
+
+  } else { 
+    newLocalTodos = JSON.parse( getTodosLocalStorage ) 
+  }
   
   const [ search, setSearch ] = React.useState( '' )
 
-  const [ todos, setTodos ] = React.useState( todosDefault )
+  const [ todos, setTodos ] = React.useState( newLocalTodos )
 
   const totalCompleted = todos.filter( todo => !!todo.completed ).length
   const total = todos.length
@@ -22,20 +34,26 @@ function App() {
   const toSearch = todos.filter( todo => regexSearch.test( todo.text ))
   let todosFilter = ( search.length >= 1 ) ? toSearch : todos
 
+  const stringifiedAndUpdateState = ( newTodos ) => {
+    const newTodosStringifing = JSON.stringify( newTodos )
+    window.localStorage.setItem( 'TODOS_V1', newTodosStringifing )
+    setTodos( newTodos )
+  }
+
   const completeTodo = ( text ) => {
     let newTodos = [ ...todos ]
     const positionTodo = newTodos.findIndex( todo => todo.text === text )
     
     todos[ positionTodo ].completed = true
 
-    setTodos( newTodos )
+    stringifiedAndUpdateState( newTodos )
   }
 
   const deleteTodo = ( text ) => {
     let newTodo = Array.from( todos )
     const positionTodo = newTodo.findIndex( todo => todo.text === text )
     newTodo.splice( positionTodo, 1 )
-    setTodos( newTodo )
+    stringifiedAndUpdateState( newTodo )
   }
   
   return (
