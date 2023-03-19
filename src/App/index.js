e933b7ba083d9ebb5cecddd9fe53b89d778221a0
rodrigo <olivertate274@gmail.com>
2023-03-18 21:24:08 -0600

    making a custom hook with useCustoHook obligation is to use use in a function

--- src/App/index.js
+++ src/App/index.js
@@ -8,23 +8,43 @@ import './App.css';
 //   { text: 'Cry with the Llorona', completed: false }
 // ]
 
-function App() {
-  debugger
-  const getTodosLocalStorage = window.localStorage.getItem( 'TODOS_V1' )
-  let newLocalTodos
+function useSaveInLocalStorage ( saveItem, valueItem ) {
+
+  const getTodosLocalStorage = window.localStorage.getItem( saveItem )
+  let saveLocalItem
 
   if( !getTodosLocalStorage ){    
     
-    window.localStorage.setItem( 'TODOS_V1', '[]' )
-    newLocalTodos = []
+    window.localStorage.setItem( saveItem, valueItem )
+    saveLocalItem = valueItem
 
-  } else { 
-    newLocalTodos = JSON.parse( getTodosLocalStorage ) 
+  } else {
+    saveLocalItem = JSON.parse( getTodosLocalStorage ) 
   }
+
+  const [ items, setItems ] = React.useState( saveLocalItem )
+
+  const stringifiedAndUpdateState = ( newTodos ) => {
+    
+    const newTodosStringifing = JSON.stringify( newTodos )
+    
+    window.localStorage.setItem( saveItem, newTodosStringifing )
+    
+    setItems( newTodos )
+  }
+
+  return [
+    items,
+    stringifiedAndUpdateState
+  ]
+
+}
+
+function App() {
+  // debugger
   
   const [ search, setSearch ] = React.useState( '' )
-
-  const [ todos, setTodos ] = React.useState( newLocalTodos )
+  const [ todos, setTodos ] = useSaveInLocalStorage( 'TODOS_V1', '[{ "text": "Cortar Cebollas", "completed": false }]' )  
 
   const totalCompleted = todos.filter( todo => !!todo.completed ).length
   const total = todos.length
@@ -34,11 +54,7 @@ function App() {
   const toSearch = todos.filter( todo => regexSearch.test( todo.text ))
   let todosFilter = ( search.length >= 1 ) ? toSearch : todos
 
-  const stringifiedAndUpdateState = ( newTodos ) => {
-    const newTodosStringifing = JSON.stringify( newTodos )
-    window.localStorage.setItem( 'TODOS_V1', newTodosStringifing )
-    setTodos( newTodos )
-  }
+  
 
   const completeTodo = ( text ) => {
     let newTodos = [ ...todos ]
@@ -46,14 +62,14 @@ function App() {
     
     todos[ positionTodo ].completed = true
 
-    stringifiedAndUpdateState( newTodos )
+    setTodos( newTodos )
   }
 
   const deleteTodo = ( text ) => {
     let newTodo = Array.from( todos )
     const positionTodo = newTodo.findIndex( todo => todo.text === text )
     newTodo.splice( positionTodo, 1 )
-    stringifiedAndUpdateState( newTodo )
+    setTodos( newTodo )
   }
   
   return (
@@ -67,8 +83,8 @@ function App() {
       setTodos={ setTodos }
       completeTodo={ completeTodo }
       deleteTodo={ deleteTodo }
-    />    
-  );
+    />
+  )
 }
 
 export default App;
